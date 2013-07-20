89db04fc32b84895b47185aa912397fa913634af
stefanprisca <stefan.prisca@gmail.com>
2013-07-20 23:31:18 +0300

    Update SnipDSLSpecificCompiler.java

--- org.eclipse.recommenders.snipeditor/src/org/eclipse/recommenders/snipeditor/compiler/SnipDSLSpecificCompiler.java
+++ org.eclipse.recommenders.snipeditor/src/org/eclipse/recommenders/snipeditor/compiler/SnipDSLSpecificCompiler.java
@@ -1,81 +1,45 @@
-package org.eclipse.recommenders.snipeditor.jvmmodel
+package org.eclipse.recommenders.snipeditor.compiler;
 
-import com.google.inject.Inject
-import org.eclipse.xtext.xbase.jvmmodel.AbstractModelInferrer
-import org.eclipse.xtext.xbase.jvmmodel.IJvmDeclaredTypeAcceptor
-import org.eclipse.xtext.xbase.jvmmodel.JvmTypesBuilder
-import org.eclipse.recommenders.snipeditor.snipDSL.domainmodel
-import org.eclipse.recommenders.snipeditor.snipDSL.entity
-import org.eclipse.recommenders.snipeditor.snipDSL.attributeDeclaration
-import org.eclipse.xtext.naming.IQualifiedNameProvider
-import org.eclipse.recommenders.snipeditor.snipDSL.method
-import org.eclipse.xtext.xbase.compiler.TypeReferenceSerializer
-import org.eclipse.recommenders.snipeditor.compiler.SnipDSLSpecificCompiler
-import org.eclipse.recommenders.snipeditor.snipDSL.impl.XVariableDeclarationImpl
+import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.recommenders.snipeditor.snipDSL.impl.XVariableDeclarationImpl;
+import org.eclipse.xtext.common.types.JvmTypeReference;
+import org.eclipse.xtext.xbase.XExpression;
+import org.eclipse.xtext.xbase.XVariableDeclaration;
+import org.eclipse.xtext.xbase.compiler.AbstractXbaseCompiler;
+import org.eclipse.xtext.xbase.compiler.IAppendable;
+import org.eclipse.xtext.xbase.compiler.XbaseCompiler;
+import org.eclipse.xtext.xbase.compiler.output.ITreeAppendable;
 
-/**
- * <p>Infers a JVM model from the source model.</p> 
- *
- * <p>The JVM model should contain all elements that would appear in the Java code 
- * which is generated from the source model. Other models link against the JVM model rather than the source model.</p>     
- */
-class SnipDSLJvmModelInferrer extends AbstractModelInferrer {
+@SuppressWarnings("restriction")
+public class SnipDSLSpecificCompiler extends XbaseCompiler {
 
-    /**
-     * convenience API to build and initialize JVM types and their members.
-     */
-	@Inject extension JvmTypesBuilder
-	@Inject extension IQualifiedNameProvider
-	@Inject extension TypeReferenceSerializer
-	@Inject SnipDSLSpecificCompiler snipCompiler
-	
-   	def dispatch void infer(entity element, IJvmDeclaredTypeAcceptor acceptor, boolean isPreIndexingPhase) {
-   		// Here you explain how your model is mapped to Java elements, by writing the actual translation code.
-   		
-   	acceptor.accept( element.toClass( element.fullyQualifiedName )
-   			 ).initializeLater [
-     			 documentation = element.documentation
-      				if (element.JType != null)
-        			superTypes += element.JType.type.cloneWithProxies 
-   				for (feature : element.features) {
-   					switch feature{
-   						attributeDeclaration case feature.JType.type!=null
-   								:{ 
-   									members+=feature.toField(feature.name, feature.JType.type)
-   								}
-   						method 
-   								:{
-   								if(feature.JType!=null){
-          						  	members += feature.toMethod(feature.name, feature.JType.type) [
-              						documentation = feature.documentation
-              						for (p : feature.params) {
-                					parameters += p.toParameter(p.name, p.JType.type)
-              						}
-             	 					body = [
-             	 						for(blkFeature : feature.body.eContents)
-             	 						{
-             	 							switch(blkFeature){
-             	 								XVariableDeclarationImpl:
-             	 									snipCompiler._toJavaStatement(blkFeature, it, true)
-             	 							}
-             	 						}
-             	 					]
-             	 					
-   								]}else{
-   									//TODO: members += feature.toMethod(feature.name, null ) [
-              						//documentation = feature.documentation
-              						/*for (p : feature.params) {
-                					parameters += p.toParameter(p.name, p.JType.type)
-              						}*/
-             	 				//body = feature.body ]
-             	 				}
-   								}
-   						
-   						}
-   					
-   				}
-   		
-   			]
-   	}
+	public void _toJavaStatement(XVariableDeclarationImpl expr, ITreeAppendable b, boolean isReferenced) {
+		if (expr.getValue() != null) {
+			//TODO: internalToJavaStatement(expr.getRight(), b, true);
+		}
+		final ITreeAppendable trace = b.trace(expr, true).trace(expr, true);
+		trace.newLine();
+		JvmTypeReference type = appendVariableTypeAndName(expr, trace);
+		trace.append(" = ");
+		/*if (expr.getValue() != null) {
+			internalToConvertedExpression((XExpression) expr.getValue(), b, type);
+		} else {*/
+			appendDefaultLiteral(trace, type);
+		//}
+		trace.append(";");
+	}
+	protected JvmTypeReference appendVariableTypeAndName(XVariableDeclarationImpl varDeclaration, ITreeAppendable b) {
+		/*if (!varDeclaration.isWriteable()) {
+			appendable.append("final ");
+		}*/
+		JvmTypeReference type = null;
+		if (varDeclaration.getJType() != null) {
+			type = varDeclaration.getJType().getType();
+		} 
+		serialize(type, varDeclaration, b);
+		b.append(" ");
+		b.append(b.declareVariable(varDeclaration, makeJavaIdentifier(varDeclaration.getName())));
+		return type;
+	}
+
 }
-
