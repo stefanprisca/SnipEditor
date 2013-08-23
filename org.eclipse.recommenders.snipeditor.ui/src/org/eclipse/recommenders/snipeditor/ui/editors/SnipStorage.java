b301966a9c723fa4f54f11e3254fbaf1f3bd30ae
stefanprisca <stefan.prisca@gmail.com>
2013-08-23 18:10:31 +0300

    Include view, not working update site yet

---
+++ org.eclipse.recommenders.snipeditor.ui/src/org/eclipse/recommenders/snipeditor/ui/editors/SnipStorage.java
@@ -0,0 +1,56 @@
+package org.eclipse.recommenders.snipeditor.ui.editors;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+
+import org.eclipse.core.resources.IStorage;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.IPersistableElement;
+import org.eclipse.ui.IStorageEditorInput;
+
+public class SnipStorage implements IStorage {
+
+    private String snipContents;
+    private File snippetStorage;
+
+    public SnipStorage(String contents) {
+        snipContents = contents;
+        // this.snippetStorage=snippet;
+    }
+
+    @Override
+    public Object getAdapter(Class adapter) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public InputStream getContents() throws CoreException {
+        // TODO Auto-generated method stub
+        return new ByteArrayInputStream(snipContents.getBytes());
+    }
+
+    @Override
+    public IPath getFullPath() {
+        // TODO Auto-generated method stub
+        IPath location = new Path(snippetStorage.getAbsolutePath());
+        return location;
+    }
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return "Snip storage";
+    }
+
+    @Override
+    public boolean isReadOnly() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+}
--- org.eclipse.recommenders.snipeditor/src/org/eclipse/recommenders/snipeditor/validation/SnipDSLValidator.java
+++ org.eclipse.recommenders.snipeditor/src/org/eclipse/recommenders/snipeditor/validation/SnipDSLValidator.java
@@ -26,135 +26,157 @@ import com.google.inject.Inject;
 @SuppressWarnings("restriction")
 public class SnipDSLValidator extends AbstractSnipDSLValidator {
 
-	@Inject
-	private ILogicalContainerProvider logicalContainerProvider;
-	
-	/**
-	 * overridden to accommodate the <i>writable</i> field
-	 */
-	@Check
-	@Override
-	public void checkVariableDeclaration(XVariableDeclaration declaration) {
-		if (declaration.getRight() == null) {
-			if (declaration.isWriteable())
-				error("Value must be initialized", Literals.XVARIABLE_DECLARATION__WRITEABLE,
-						ValidationMessageAcceptor.INSIGNIFICANT_INDEX, MISSING_INITIALIZATION);
-		/*	if (declaration.getType() == null)
-				error("Type cannot be derived", Literals.XVARIABLE_DECLARATION__NAME,
-						ValidationMessageAcceptor.INSIGNIFICANT_INDEX, MISSING_TYPE);*/
-		}
-	}
-	
-	/**
-	 * Check for correct feature calls.
-	 * <p>
-	 * 	<li>if caller is not instantiated and a member feature is called, 
-	 * 	prompt error</li> 
-	 * 
-	 * 	<li>if a call to a method is made without explicit feature call <i>()</i>
-	 * prompt error</li>
-	 * </p>
-	 * @param featureCall
-	 */
-	@Check
-	void checkCorrectFeatureCall(XMemberFeatureCall featureCall) {
-		if(featureCall.getActualReceiver() instanceof XFeatureCall){
-			XFeatureCall caller= (XFeatureCall)featureCall.getActualReceiver();
-			if(caller.getFeature() instanceof XVariableDeclaration){
-				//System.out.println("it's a variable declaration");
-				XVariableDeclaration varCaller=(XVariableDeclaration)caller.getFeature();
-				if(varCaller.getRight()==null){
-					error("Element has not been instantiated! \n"
-							+ "Cannot access a null element!",Literals.XABSTRACT_FEATURE_CALL__FEATURE);
-					
-				}
-			}else if(caller.getFeature() instanceof attributeDeclaration){
-				//System.out.println("it's a variable declaration");
-				attributeDeclaration varCaller=(attributeDeclaration)caller.getFeature();
-				if(varCaller.getValue()==null){
-					error("Element has not been instantiated! \n"
-							+ "Cannot access a null element!",Literals.XABSTRACT_FEATURE_CALL__FEATURE);
-					
-				}
-			}
-			//System.out.println(caller.getFeature().eClass());
-		}
-		//System.out.println(featureCall.getActualReceiver().eClass());
-		if (!featureCall.isExplicitOperationCall() && (featureCall.getFeature() instanceof JvmOperation))
-		{
-			error("Please provide full feature call",Literals.XABSTRACT_FEATURE_CALL__FEATURE);
-			System.out.println(featureCall.getMemberCallTarget());
-		}
-		
-	}
-	
-	/**
-	 * Check if assignment if made to final variable
-	 * Overridden to accommodate <i>writable</i> field
-	 */
-	@Check
-	@Override
-	public void checkAssignment(XAssignment assignment) {
-		JvmIdentifiableElement assignmentFeature = assignment.getFeature();
-		if (assignmentFeature instanceof XVariableDeclaration
-				&& ((XVariableDeclaration) assignmentFeature).isWriteable()){
-			error("Assignment to final variable", Literals.XASSIGNMENT__ASSIGNABLE,
-					ValidationMessageAcceptor.INSIGNIFICANT_INDEX, ASSIGNMENT_TO_FINAL);
-		
-		}
-		else if (assignmentFeature instanceof JvmFormalParameter)
-			error("Assignment to final parameter", Literals.XASSIGNMENT__ASSIGNABLE,
-					ValidationMessageAcceptor.INSIGNIFICANT_INDEX, ASSIGNMENT_TO_FINAL);
-		else if (assignmentFeature instanceof JvmField && ((JvmField) assignmentFeature).isFinal()) {
-			JvmField field = (JvmField) assignmentFeature;
-			JvmIdentifiableElement container = logicalContainerProvider.getNearestLogicalContainer(assignment);
-			
-			// don't issue an error if it's an assignment of a local final field within a constructor.
-			if (container != null && container instanceof JvmConstructor) {
-				JvmConstructor constructor = (JvmConstructor) container;
-				if (field.getDeclaringType() == constructor.getDeclaringType())
-					return;
-			}
-			error("Assignment to final field", Literals.XASSIGNMENT__ASSIGNABLE,
-					ValidationMessageAcceptor.INSIGNIFICANT_INDEX, ASSIGNMENT_TO_FINAL);
-		}
-	}
-	/**
-	 * Override to eliminate checking for side effects
-	 * Not sure if it is good
-	 * TODO: take a deeper look into the effects
-	 */
-	@Override
-	@Check
-	public void checkInnerExpressions(XExpression expr) {
-		
-	}
-	/**
-	 * Override to check for <b>DeclaredFormalParameters</b>
-	 * The super checks for other parameter type and promts error
-	 * TODO: get a better understanding of the mechanics
-	 */
-	@Override
-	@Check
-	public void checkClosureParameterTypes(XClosure closure) {
-		if (closure.getDeclaredFormalParameters().isEmpty())
-			return;
-		super.checkClosureParameterTypes(closure);
-	}
-	
-	/**
-	 * check if there are different types in a block assignment
-	 * @param blkAssign
-	 */
-	@Check
-	public void checkValidBlockAssignment(blockAssignment blkAssign){
-		for(int i=1; i<blkAssign.getValues().size(); i++){
-			if(!blkAssign.getValues().get(i).getClass().equals(blkAssign.getValues().get(i-1).getClass())){
-				error("Different types in block!", blkAssign.getValues().get(i).eContainingFeature());
-			}
-		}
-	}
-	
-	
-	
+    @Inject
+    private ILogicalContainerProvider logicalContainerProvider;
+
+    /**
+     * overridden to accommodate the <i>writable</i> field
+     */
+    @Check
+    @Override
+    public void checkVariableDeclaration(XVariableDeclaration declaration) {
+        if (declaration.getRight() == null) {
+            if (declaration.isWriteable())
+                error("Value must be initialized",
+                        Literals.XVARIABLE_DECLARATION__WRITEABLE,
+                        ValidationMessageAcceptor.INSIGNIFICANT_INDEX,
+                        MISSING_INITIALIZATION);
+            /*
+             * if (declaration.getType() == null)
+             * error("Type cannot be derived",
+             * Literals.XVARIABLE_DECLARATION__NAME,
+             * ValidationMessageAcceptor.INSIGNIFICANT_INDEX, MISSING_TYPE);
+             */
+        }
+    }
+
+    /**
+     * Check for correct feature calls.
+     * <p>
+     * <li>if caller is not instantiated and a member feature is called, prompt
+     * error</li>
+     * 
+     * <li>if a call to a method is made without explicit feature call <i>()</i>
+     * prompt error</li>
+     * </p>
+     * 
+     * @param featureCall
+     */
+    @Check
+    void checkCorrectFeatureCall(XMemberFeatureCall featureCall) {
+        if (featureCall.getActualReceiver() instanceof XFeatureCall) {
+            XFeatureCall caller = (XFeatureCall) featureCall
+                    .getActualReceiver();
+            if (caller.getFeature() instanceof XVariableDeclaration) {
+                // System.out.println("it's a variable declaration");
+                XVariableDeclaration varCaller = (XVariableDeclaration) caller
+                        .getFeature();
+                if (varCaller.getRight() == null) {
+                    error("Element has not been instantiated! \n"
+                            + "Cannot access a null element!",
+                            Literals.XABSTRACT_FEATURE_CALL__FEATURE);
+
+                }
+            } else if (caller.getFeature() instanceof attributeDeclaration) {
+                // System.out.println("it's a variable declaration");
+                attributeDeclaration varCaller = (attributeDeclaration) caller
+                        .getFeature();
+                if (varCaller.getValue() == null) {
+                    error("Element has not been instantiated! \n"
+                            + "Cannot access a null element!",
+                            Literals.XABSTRACT_FEATURE_CALL__FEATURE);
+
+                }
+            }
+            // System.out.println(caller.getFeature().eClass());
+        }
+        // System.out.println(featureCall.getActualReceiver().eClass());
+        if (!featureCall.isExplicitOperationCall()
+                && (featureCall.getFeature() instanceof JvmOperation)) {
+            error("Please provide full feature call",
+                    Literals.XABSTRACT_FEATURE_CALL__FEATURE);
+            System.out.println(featureCall.getMemberCallTarget());
+        }
+
+    }
+
+    /**
+     * Check if assignment if made to final variable Overridden to accommodate
+     * <i>writable</i> field
+     */
+    @Check
+    @Override
+    public void checkAssignment(XAssignment assignment) {
+        JvmIdentifiableElement assignmentFeature = assignment.getFeature();
+        if (assignmentFeature instanceof XVariableDeclaration
+                && ((XVariableDeclaration) assignmentFeature).isWriteable()) {
+            error("Assignment to final variable",
+                    Literals.XASSIGNMENT__ASSIGNABLE,
+                    ValidationMessageAcceptor.INSIGNIFICANT_INDEX,
+                    ASSIGNMENT_TO_FINAL);
+
+        } else if (assignmentFeature instanceof JvmFormalParameter)
+            error("Assignment to final parameter",
+                    Literals.XASSIGNMENT__ASSIGNABLE,
+                    ValidationMessageAcceptor.INSIGNIFICANT_INDEX,
+                    ASSIGNMENT_TO_FINAL);
+        else if (assignmentFeature instanceof JvmField
+                && ((JvmField) assignmentFeature).isFinal()) {
+            JvmField field = (JvmField) assignmentFeature;
+            JvmIdentifiableElement container = logicalContainerProvider
+                    .getNearestLogicalContainer(assignment);
+
+            // don't issue an error if it's an assignment of a local final field
+            // within a constructor.
+            if (container != null && container instanceof JvmConstructor) {
+                JvmConstructor constructor = (JvmConstructor) container;
+                if (field.getDeclaringType() == constructor.getDeclaringType())
+                    return;
+            }
+            error("Assignment to final field",
+                    Literals.XASSIGNMENT__ASSIGNABLE,
+                    ValidationMessageAcceptor.INSIGNIFICANT_INDEX,
+                    ASSIGNMENT_TO_FINAL);
+        }
+    }
+
+    /**
+     * Override to eliminate checking for side effects Not sure if it is good
+     * TODO: take a deeper look into the effects
+     */
+    @Override
+    @Check
+    public void checkInnerExpressions(XExpression expr) {
+
+    }
+
+    /**
+     * Override to check for <b>DeclaredFormalParameters</b> The super checks
+     * for other parameter type and promts error TODO: get a better
+     * understanding of the mechanics
+     */
+    @Override
+    @Check
+    public void checkClosureParameterTypes(XClosure closure) {
+        if (closure.getDeclaredFormalParameters().isEmpty())
+            return;
+        super.checkClosureParameterTypes(closure);
+    }
+
+    /**
+     * check if there are different types in a block assignment
+     * 
+     * @param blkAssign
+     */
+    @Check
+    public void checkValidBlockAssignment(blockAssignment blkAssign) {
+        for (int i = 1; i < blkAssign.getValues().size(); i++) {
+            if (!blkAssign.getValues().get(i).getClass()
+                    .equals(blkAssign.getValues().get(i - 1).getClass())) {
+                error("Different types in block!", blkAssign.getValues().get(i)
+                        .eContainingFeature());
+            }
+        }
+    }
+
 }
